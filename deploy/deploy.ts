7d05101a3ab6be53f9097b70372aec7606901fff
lilywardx <sjhdfjhsb0552@hotmail.com>
2025-12-23 17:57:58 +0800

    build: update hardhat deploy flow

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -2,16 +2,20 @@ import { DeployFunction } from "hardhat-deploy/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  if (hre.network.name === "sepolia" && (!process.env.PRIVATE_KEY || process.env.PRIVATE_KEY.length === 0)) {
+    throw new Error("PRIVATE_KEY env variable is required for sepolia deployments");
+  }
+
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const deployedGame = await deploy("CryptoVeilGame", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  console.log(`CryptoVeilGame contract: `, deployedGame.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_crypto_veil_game"; // id required to prevent reexecution
+func.tags = ["CryptoVeilGame"];
